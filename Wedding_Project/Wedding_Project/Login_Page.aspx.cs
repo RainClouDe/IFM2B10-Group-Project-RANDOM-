88f4c29c414f3faff72d65dd0c8cd1d92db4efa1
abc <asd @ h.com>
2016-11-02 22:57:26 +0200

    Fixed the shopping cart now works. Also added Home link to master page. Thanks Ivan.

--- Wedding_Project/Wedding_Project/Login_Page.aspx.cs
+++ Wedding_Project/Wedding_Project/Login_Page.aspx.cs
@@ -31,7 +31,7 @@ namespace Wedding_Project
                 int clientID = proxy.returnClientId(loginID);
 
                 HttpContext.Current.Session["ClientId"] = clientID;
-                Response.Redirect("Wedding_Welcome_Card.aspx");
+                Response.Redirect("Home_Page.aspx");
                 //Redirect 
             }
             else
--- Wedding_Project/Wedding_Project/Shopping_Cart_Page.aspx.cs
+++ Wedding_Project/Wedding_Project/Shopping_Cart_Page.aspx.cs
@@ -26,7 +26,7 @@ namespace Wedding_Project
         {
             ServiceReference1.Service1Client proxy = new ServiceReference1.Service1Client();
             ServiceReference1.Cart_Table[] tableCartList;
-            int loggedInClientID = 0;
+            int loggedInClientID;
 
             if (HttpContext.Current.Session["ClientLoggedIn"] != null)
             {
@@ -36,13 +36,11 @@ namespace Wedding_Project
 
                     loggedInClientID = (int)HttpContext.Current.Session["ClientId"];
 
-                    //MessageBox.show(this, loggedInClientID.ToString());
-
                     proxy.addToCartTable(loggedInClientID, decorID, 0, 5, null);
 
                 }
-
-                tableCartList = proxy.GetListOfCartItems(loggedInClientID);
+                
+                tableCartList = proxy.GetListOfCartItems((int)HttpContext.Current.Session["ClientId"]);
 
                 for (int i = 0; i < tableCartList.Length; i++)
                 {
